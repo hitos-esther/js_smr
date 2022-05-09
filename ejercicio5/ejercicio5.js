788dcd798e047460b6fa8e9078e3b72efba344bc
estherhitos <esther.hitos@iescampanillas.com>
2022-05-09 13:19:36 +0200

    ejercicio 5 terminado con value

---
+++ ejercicio5/ejercicio5.js
@@ -0,0 +1,30 @@
+/** JavaScript para el ejercicio 5 
+ * Detecta Cadena de caracteres, es mayúsculas o minúsculas 
+ * Authora: Esther Hitos Garcia
+ * */
+
+
+function mayMin() {
+
+    
+    let cadena = document.getElementById('cadena').value;//cadena original
+    let resultado ="" ; //"La cadena \""+resultado+"\" "
+    let cadena2= null;//cadena original vacia objeto null
+
+    
+    // Comprobar mayúsculas y minúsculas
+    if(cadena === cadena.toUpperCase()) {
+      resultado += " en mayúsculas";
+    }
+    else if(cadena === cadena.toLowerCase()) {
+      resultado += " en minúsculas";
+    }
+    else {
+      resultado += " en mayúsculas y minúsculas";
+    }
+  
+    return document.getElementById('resultado').innerHTML = "La Cadena esta escrita : "+resultado;
+    
+  }
+  
+
