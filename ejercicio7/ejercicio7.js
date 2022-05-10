516fb767ad6a59711fbcbe5752b3525efa2fe048
estherhitos <esther.hitos@iescampanillas.com>
2022-05-10 18:24:19 +0200

    Commit de ejercicio6 Palindromo

---
+++ ejercicio7/ejercicio7.js
@@ -0,0 +1,19 @@
+/**
+ * JavaScript de Ejercicio 7
+ * 
+ * Authora: Esther Hitos Garcia
+ */
+
+function divisionEuclides() {
+    
+    let divi =Number (document.getElementById('dividendo').value);
+    let divisor =Number( document.getElementById('divisor').value);
+    let cociente =0;
+    while (divi>=divisor){
+
+       divi -= divisor;
+       cociente++;      
+    }
+
+    document.getElementById('resultadoDivision').innerHTML = "El cociente es <strong>"+cociente+"</strong> y el resto <strong>"+divi+"</strong>";
+}
