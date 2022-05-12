579dff5aebd5966133058f116fd2d2cb768bd983
estherhitos <esther.hitos@iescampanillas.com>
2022-05-12 11:21:36 +0200

    Ejercicio10 con JSON

---
+++ ejercicio10/ejercicio10.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ * 
+ * 
+ */
+ function TraducirNombre(){
+
+    const meses = {"Enero":"January",
+                    "Febrero":"February",
+                    "Marzo":"March",
+                    "Abril":"April",
+                    "Mayo":"March",
+                    "Junio":"Juny",
+                    "Julio":"July",
+                    "Agosto":"August",
+                    "Septiembre":"September",
+                    "Octubre":"October",
+                    "Nobiembre":"November",
+                    "Diciembre":"December"}
+
+    let mes = document.getElementById("mes").value;
+
+ 
+    //let respuesta ="";
+
+    let respuesta = (mes == "error")? "no has elegido mes. ": meses[mes];//hay que poner la variable
+   /* if(numeroMes==-1)
+        respuesta= "No has elegido numero";
+    else
+        respuesta = meses[numeroMes];*/
+    
+
+    document.getElementById("nombreMes").innerHTML=respuesta;
+}
