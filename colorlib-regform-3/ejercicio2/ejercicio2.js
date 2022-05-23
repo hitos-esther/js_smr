2085ad88e0df20da0f8086c979e88c29fbede3cb
estherhitos <esther.hitos@iescampanillas.com>
2022-05-23 18:44:49 +0200

    Cambio del ejercicio 2

--- colorlib-regform-3/ejercicio2/ejercicio2.js
+++ colorlib-regform-3/ejercicio2/ejercicio2.js
@@ -1,5 +1,14 @@
 /* javascript para el ejercicio 2 de portfolio de lenguajes de marcas */
 
+//1º forma del boton
+//indicamos al pricipio del JS que evento dispara que funcion utilizando el arbol DOM
+//document.getElementById("calcular").onclick= /**aqui vendria la function calcular */
+//esto se hace cuando la funcion es muy corta (onclick="calcular()")
+
+//2º forma del boton mas optima con addEventListener cogiendo el id del boton
+document.getElementById('calcular').addEventListener("click",calcular);
+
+
 function calcular(){
 
     // obtener dato (radio)
@@ -14,10 +23,33 @@ function calcular(){
     // mostrar resultados
 
     // te atreves a refactorizar la salida de resultados?????
+//primero nodo li creado y engacharlo al original
+//1º crear nodo li
+       let nuevoNodo=document.createElement("li");
+       //2º meter contenido en el nodo ose el resultado
+       nuevoNodo.innerHTML = "La longitud de la circunferencia con radio"+radio+" es : "+circunferencia.toFixed(2);
+       //3º enganchar el nodo en la lista padre 
+       document.getElementById("resultados").appendChild(nuevoNodo);
+    
+       nuevoNodo=document.createElement("li");
+       //2º meter contenido en el nodo ose el resultado
+       nuevoNodo.innerHTML = "El área del círculo con radio"+radio+" es : "+circulo.toFixed(2);
+       //3º enganchar el nodo en la lista padre 
+       document.getElementById("resultados").appendChild(nuevoNodo);
+       
+       nuevoNodo=document.createElement("li");
+       //2º meter contenido en el nodo ose el resultado
+       nuevoNodo.innerHTML = "El volumen de la esfera con radio"+radio+" es : "+esfera.toFixed(2);
+       //3º enganchar el nodo en la lista padre 
+       document.getElementById("resultados").appendChild(nuevoNodo);
 
+
+
+/*
     document.getElementById("circunferencia").innerHTML="La longitud de la circunferencia es : "+circunferencia.toFixed(2);
 
     document.getElementById("circulo").innerHTML="El área del círculo es : "+circulo.toFixed(2);
 
     document.getElementById("esfera").innerHTML="El volumen de la esfera es : "+esfera.toFixed(2);
+    */
 }
